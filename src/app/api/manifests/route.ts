71b0db62d78b7510319dc82e0bfdf63a6eaafd32
FathurAhmad <fathurahmad560@gmail.com>
2026-05-29 12:16:48 +0700

    Revert "api manifests add filter: status"
    This reverts commit ff96993580886882368c8e49d58542a866e954d6.

--- src/app/api/manifests/route.ts
+++ src/app/api/manifests/route.ts
@@ -2,13 +2,9 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { ManifestStatus } from "@prisma/client";
 
-export async function GET(request: Request) {
-
+export async function GET() {
     try {
-        const { searchParams } = new URL(request.url);
-        const manifestStatus = searchParams.get('status');
         const manifests = await prisma.manifests.findMany({
-            where: { status: manifestStatus as ManifestStatus },
             orderBy: { manifest_number: 'asc'},
             include: {
                 manifest_items: true,
